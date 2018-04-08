5a40f96a528ce356aca96cbe30b211f2748c5837
Lee Chin Yong <melvinlee@hotmail.com>
2018-04-08 16:58:33 +0800

    feat: implemented findBookById function.

--- handler.js
+++ handler.js
@@ -5,20 +5,6 @@ let lambda = new AWS.Lambda();
 
 console.log("Loading...");
 
-module.exports.findBookById = (event, context, callback) => {
-  console.log(`Request: ${JSON.stringify(event, null, 2)}`);
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-
-  callback(null, response);
-};
-
 module.exports.editBook = (event, context, callback) => {
   console.log(`Request: ${JSON.stringify(event, null, 2)}`);
 
