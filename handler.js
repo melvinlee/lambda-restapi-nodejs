5c339931ef08f580c58cf3dbdfa4d509a7da6f8e
Lee Chin Yong <melvinlee@hotmail.com>
2018-04-08 16:45:12 +0800

    feat: implmented deleteBook function.

--- handler.js
+++ handler.js
@@ -32,17 +32,3 @@ module.exports.editBook = (event, context, callback) => {
 
   callback(null, response);
 };
-
-module.exports.removeBook = (event, context, callback) => {
-  console.log(`Request: ${JSON.stringify(event, null, 2)}`);
-
-  const response = {
-    statusCode: 204,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-
-  callback(null, response);
-};
