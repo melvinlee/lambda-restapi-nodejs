2efc4e1f715a8b40da71668c85d3e2c67e760a5c
Lee Chin Yong <melvinlee@hotmail.com>
2018-05-04 15:40:07 +0800

    build: added build script.

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+instruction()
+{
+    echo "usage: ./build.sh deploy <env>"
+    echo 
+    echo "env: eg. dev, staging, prod, ..."
+    echo 
+    echo "for example: ./bulid.sh dev"
+}
+
+if [ $# -eq 0 ]; then
+    instruction
+    exit 1
+elif [ "$1" = "deploy" ] && [ $# -eq 2 ]; then
+    STAGE=$2
+
+    echo "Installing dependencies ..."
+    npm install
+    echo
+    echo "Deploying to evn: $STAGE..."
+    sls deploy --stage $STAGE 
+    echo
+else
+    instruction
+    exit 1
+fi
