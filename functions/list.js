0bca6ec7a3b82a76301d89615704e21e8e24220d
Lee Chin Yong <melvinlee@hotmail.com>
2018-05-06 21:32:50 +0800

    fix: fixed variable.

--- functions/list.js
+++ functions/list.js
@@ -6,7 +6,7 @@ module.exports.handler = async (event, context, callback) => {
   try {
     let result = await databaseAccess.getAllBooks();
     callback(null, response.buildResponse(200, result.Items));
-  } catch (error) {
+  } catch (err) {
     callback(null, response.buildResponse(501, { message: "Couldn't fetch books item.", error: err }));
   }
 };
